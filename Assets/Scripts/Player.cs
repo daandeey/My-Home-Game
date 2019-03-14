da7c9701cb44aa9a984e11819ef868fd25d39227
budiman <hafizh.budiman@gmail.com>
2019-03-14 16:49:16 +0700

    add jump and ground collision

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,38 +4,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float jumpVelocity;
     public Vector2 velocity;
+    public float gravity;
     public LayerMask wallMask;
+    public LayerMask floorMask;
     private bool walk, walk_left, walk_right, jump;
 
+    public enum PlayerState {
+        jumping,
+        idle,
+        walking
+    }
+
+    private PlayerState playerstate = PlayerState.idle;
+
+    private bool grounded = false;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        //Fall();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         CheckPlayerInput();
         UpdatePlayerPosition();
     }
 
-    void UpdatePlayerPosition()
-    {
+    void UpdatePlayerPosition() {
         Vector3 pos = transform.localPosition;
         Vector3 scale = transform.localScale;
 
-        if (walk) 
-        {
-            if (walk_left)
-            {
+        if (walk) {
+            if (walk_left) {
                 pos.x -= velocity.x * Time.deltaTime;
                 scale.x = -1;
             }
-            if (walk_right)
-            {
+            if (walk_right) {
                 pos.x += velocity.x * Time.deltaTime;
                 scale.x = 1;
             }
@@ -43,12 +49,27 @@ public class Player : MonoBehaviour
             pos = CheckWallRays(pos, scale.x);
         }
 
+        if (jump && playerstate != PlayerState.jumping) {
+
+            playerstate = PlayerState.jumping;
+
+            velocity = new Vector2(velocity.x, jumpVelocity);
+        }
+
+        if (playerstate == PlayerState.jumping) {
+
+            pos.y += velocity.y * Time.deltaTime;
+
+            velocity.y -= gravity * Time.deltaTime;
+        }
+
+        if (velocity.y <= 0) pos = CheckFloorRays(pos);
+
         transform.localPosition = pos;
         transform.localScale = scale;
     }
 
-    void CheckPlayerInput()
-    {
+    void CheckPlayerInput() {
         bool input_left = Input.GetKey(KeyCode.LeftArrow);
         bool input_right = Input.GetKey(KeyCode.RightArrow);
         bool input_space = Input.GetKey(KeyCode.Space);
@@ -74,5 +95,53 @@ public class Player : MonoBehaviour
         }
 
         return pos;
+    }
+
+    Vector3 CheckFloorRays (Vector3 pos) {
+
+        Vector2 originLeft = new Vector2 (pos.x - 0.5f + 0.2f, pos.y - 1f);
+        Vector2 originMiddle = new Vector2 (pos.x, pos.y - 1f);
+        Vector2 originRight = new Vector2 (pos.x + 0.5f - 0.2f, pos.y - 1f);
+
+        RaycastHit2D floorLeft = Physics2D.Raycast (originLeft, Vector2.down, velocity.y * Time.deltaTime, floorMask);
+        RaycastHit2D floorMiddle = Physics2D.Raycast (originMiddle, Vector2.down, velocity.y * Time.deltaTime, floorMask);
+        RaycastHit2D floorRight = Physics2D.Raycast (originRight, Vector2.down, velocity.y * Time.deltaTime, floorMask);
+
+        if (floorLeft.collider != null || floorMiddle.collider != null || floorRight.collider != null) {
+
+            RaycastHit2D hitRay = floorRight;
+
+            if (floorLeft) {
+                hitRay = floorLeft;
+            } else if (floorMiddle) {
+                hitRay = floorMiddle;
+            } else if (floorRight) {
+                hitRay = floorRight;
+            }
+
+            playerstate = PlayerState.idle;
+
+            grounded = true;
+
+            velocity.y = 0;
+
+            pos.y = hitRay.collider.bounds.center.y + hitRay.collider.bounds.size.y / 2 + 1;
+        } else {
+
+            if (playerstate != PlayerState.jumping) {
+
+                Fall();
+            }
+        }
+
+        return pos;
+    }
+
+    void Fall() {
+        velocity.y = 0;
+
+        playerstate = PlayerState.jumping;
+
+        grounded = false;
     }
 }
