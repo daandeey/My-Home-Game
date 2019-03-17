7e1f35a704329c402107df571332c54e78123342
daandeey <dandyarifrahman@gmail.com>
2019-03-17 13:52:46 +0700

    Added Win Scenes

---
+++ Assets/Win.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    public void PlayAgain ()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void GoToMainMenu ()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
