19c79d07275c867a1326a689b899e9d83ccb55e9
Restu Wahyu Kartiko <13516155@std.stei.itb.ac.id>
2019-03-18 19:07:43 +0700

    Add send notification to android

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
 
 public class Win : MonoBehaviour
 {
@@ -25,6 +26,46 @@ public class Win : MonoBehaviour
 
     void Start () {
         score = PlayerPrefs.GetInt("Player Score");
+        if(score >= 500)
+        {
+            StartCoroutine(request());
+        }
+
         SetScoreText();
+    }
+
+    IEnumerator request()
+    {
+        int nominal = score / 500 * 10000;
+        score = score % 500;
+        
+        WWWForm form = new WWWForm();
+        form.AddField("tipe", 2);
+        form.AddField("nominal", nominal);
+
+        string url = "https://myhome-be.herokuapp.com/notifications/";
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Response res = JsonUtility.FromJson<Response>(www.downloadHandler.text);
+
+                if (res.success)
+                {
+                    Debug.Log("Notification sent");
+                }
+                else
+                {
+                    Debug.Log("Faild to post request");
+                }
+            }
+        }
     }
 }
